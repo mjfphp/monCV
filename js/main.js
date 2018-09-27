83b0f8a06aa53ff0ab8e74f94dcd17dade728049
Mjamai <mjf.php@gmail.com>
2018-09-27 14:20:22 +0200

    partie 1 TP Javascript

--- js/main.js
+++ js/main.js
@@ -1,42 +1,24 @@
-$(document).ready( function() {
 
-	// Logo
-	var $logo 	= $('#logo');
-	 if (location.href.indexOf("#") != -1) {
-        if(location.href.substr(location.href.indexOf("#"))!='#about'){
-        	$logo.show();
-        }
+
+
+
+let cacher=(e)=>{
+
+    let xu=document.getElementById(e)
+    let x=xu.nextElementSibling;
+   
+
+    console.log(e);
+     if (x.style.display === "none") {
+
+        x.style.display= "block";
+       
+    } else {
+        x.style.display = "none";
+        console.log(xu.childNodes[0]);
+      
+        
     }
     
-	// Show logo 
-	$('#tab-container .tab a').click(function() {
-	  $logo.slideDown('slow');
-	});
-	// Hide logo
-	$('#tab-about').click(function() {
-	  $logo.slideUp('slow');
-	});	
-function animMeter(){
-    $(".meter > span").each(function() {
-                $(this)
-                    .data("origWidth", $(this).width())
-                    .width(0)
-                    .animate({
-                        width: $(this).data("origWidth")
-                    }, 1200);
-            });
-}
-animMeter();
-
-      $('#tab-container').easytabs({
-        animate			: true,
-        updateHash		: true,
-        transitionIn	: 'slideDown',
-        transitionOut	: 'slideUp',
-        animationSpeed	: 600,
-        tabActiveClass	: 'active'}).bind('easytabs:midTransition', function(event, $clicked, $targetPanel){
-            if($targetPanel.selector=='#resume'){
-                    animMeter();
-            }
-        });
-    });
+    
+};
